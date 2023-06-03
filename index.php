da90f50072a9152095975fbaef38e7354f489236
Janak kr shrestha <janakshresthapro@gmail.com>
2023-06-03 09:20:45 +0545

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!-- https://youtu.be/nn4HSEaZhnI -->
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Home</title>
+</head>
+<body>
+
+	<h1>Home page</h1>
+
+	<?php include('header.php')?>
+	
+</body>
+</html>
