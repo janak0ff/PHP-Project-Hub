3b074eb27479af818bcf44e623feae6fe8d7bb7c
Janak kr shrestha <janakshresthapro@gmail.com>
2023-06-05 09:32:53 +0545

    fix

--- forgot-password.php
+++ forgot-password.php
@@ -50,7 +50,7 @@ if (isset($_POST['submit'])) {
                 //Content
                 $mail->isHTML(true);                                  //Set email format to HTML
                 $mail->Subject = 'no reply';
-                $mail->Body    = 'Here is the verification link <b><a href="http://localhost/login/change-password.php?reset='.$code.'">http://localhost/login/change-password.php?reset='.$code.'</a></b>';
+                $mail->Body    = 'Here is the verification link <b><a href="http://localhost/complete-login-register-form-with-email-verification/change-password.php?reset='.$code.'">http://localhost/login/change-password.php?reset='.$code.'</a></b>';
 
                 $mail->send();
                 echo 'Message has been sent';
--- register.php
+++ register.php
@@ -44,8 +44,8 @@
                         $mail->isSMTP();                                            //Send using SMTP
                         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
                         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-                        $mail->Username   = 'janakshresthapro@gmail.com';                     //SMTP username
-                        $mail->Password   = 'ijyyndkinemell';                               //SMTP password
+                        $mail->Username   = 'janakshrkaowaesthapro@gmail.com';                     //SMTP username
+                        $mail->Password   = 'ijyyndkikaowanemellpc';                               //SMTP password
                         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
                         $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
